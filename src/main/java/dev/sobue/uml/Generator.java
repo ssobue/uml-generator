17d3819afe6dcc36c099316c9d00929deebfe383
SOBUE Sho <sho@sobue.dev>
2021-01-21 21:06:02 +0900

    executable jar

--- src/main/java/dev/sobue/uml/Generator.java
+++ src/main/java/dev/sobue/uml/Generator.java
@@ -1,8 +1,19 @@
 package dev.sobue.uml;
 
+/**
+ * Main Class.
+ *
+ * @author ssobue
+ * @since 1.0
+ */
 public class Generator {
 
+  /**
+   * Main Method.
+   *
+   * @param args Command Line Arguments
+   */
   public static void main(String[] args) {
-
+    throw new UnsupportedOperationException();
   }
 }
